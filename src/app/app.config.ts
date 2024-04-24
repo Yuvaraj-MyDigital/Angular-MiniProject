818c321debbf5eb69964c6a9da150f9ed2094826
Yuvaraj-MyDigital <yuvaraj.senthil@mydigitalaccounts.com>
2024-04-24 20:04:58 +0530

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule, provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideAnimations(), 
+    provideToastr(),
+    importProvidersFrom(ToastrModule.forRoot({
+      positionClass:"toast-top-center",
+      timeOut: 2000,}))
+]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/users/register/register.component';
+import { SetPasswordComponent } from './components/users/set-password/set-password.component';
+import { LoginComponent } from './components/users/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "register", pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
+    {path: 'login', component: LoginComponent },
+    { path: 'set-password/:token', component: SetPasswordComponent },
+    { path: '**', component: RegisterComponent }
+
+];
